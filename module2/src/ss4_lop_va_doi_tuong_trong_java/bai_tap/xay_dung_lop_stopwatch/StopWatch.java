06c60005a45c790f64ac87f3cc60af26ca0eb898
Kenzuro0305 <kenzuro0305@gmail.com>
2021-10-27 16:51:57 +0700

    up

---
+++ module2/src/ss4_lop_va_doi_tuong_trong_java/bai_tap/xay_dung_lop_stopwatch/StopWatch.java
@@ -0,0 +1,35 @@
+package ss4_lop_va_doi_tuong_trong_java.bai_tap.xay_dung_lop_stopwatch;
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public StopWatch(long startTime, long endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return getEndTime() - getStartTime();
+    }
+}
