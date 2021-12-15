059ad787a992c547f96b0d6c1a33e693397323e6
Kenzuro0305 <kenzuro0305@gmail.com>
2021-12-15 16:55:15 +0700

    up

---
+++ module2/src/ss5/bai_tap/access_modifier/Main.java
@@ -0,0 +1,9 @@
+package ss5_access_modifier_static_method_static_property.bai_tap.access_modifier;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(10);
+        System.out.println("Radius: "+circle1.getRadius());
+        System.out.println("Area: "+circle1.getArea());
+    }
+}
