3baa541b07cdcc441917a7186c1278a7f52d1fe0
Kenzuro0305 <kenzuro0305@gmail.com>
2022-04-27 20:38:29 +0700

    up

---
+++ module3/s11/thuc_hanh/src/main/java/controller/CustomerServlet.java
@@ -0,0 +1,23 @@
+package controller;
+
+import service.CustomerService;
+import service.impl.ICustomerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CustomerServlet")
+public class CustomerServlet extends HttpServlet {
+    private CustomerService customerService = new ICustomerService();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
